c6bcdf36b6ffac668e913a69534b57e1d6ecb151
Joyruchie <joyirowhueli69@yahoo.com>
2023-05-02 17:32:43 -0700

    task10

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +13,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void _putchar(char c);
+
+#endif

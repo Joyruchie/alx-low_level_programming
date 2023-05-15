f96bb1089f2f4ef561a5f2baff12485527acf009
Joyruchie <joyirowhueli69@yahoo.com>
2023-05-15 13:01:16 -0700

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-
 /**
- * main - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Program that prings all arguments it receives
+ * @argc: Number of argument passed into main function
+ * @argv: Vector of arguments passed into main function
  *
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
-
 {
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
 
